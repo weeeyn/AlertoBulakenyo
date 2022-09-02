250fedaa253d4424a4d421737b73930507424d46
weeeyn <rabayareynalyn03@gmail.com>
2022-09-03 01:56:41 +0800

    disaster

--- app/src/main/java/com/activity/alertobulakenyo/EQ_info.java
+++ app/src/main/java/com/activity/alertobulakenyo/EQ_info.java
@@ -1,21 +1,17 @@
 package com.activity.alertobulakenyo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class Earthquake extends AppCompatActivity {
+public class EQ_info extends AppCompatActivity {
 
-    Button btnSamp;
-    ImageButton btnBackDisaster;
-    RecyclerView rvDisEQ;
+    ImageButton btnBackEQ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,40 +22,26 @@ public class Earthquake extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION); //enable full screen
 
-        setContentView(R.layout.activity_earthquake);
+        setContentView(R.layout.activity_eq_info);
 
-        btnBackDisaster = (ImageButton) findViewById (R.id.btnBackDisaster);
-        btnSamp = (Button) findViewById (R.id.btnSamp);
+        btnBackEQ = (ImageButton) findViewById (R.id.btnBackEQ);
 
-        rvDisEQ = (RecyclerView) findViewById (R.id.rvDisEQ);
-
-        btnBackDisaster.setOnClickListener(new View.OnClickListener() {
+        btnBackEQ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Earthquake.this, Disaster.class);
+                Intent intent = new Intent(EQ_info.this, Earthquake.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_in_left,
                         R.anim.slide_out_right);
             }
         });
-
-        btnSamp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Earthquake.this, EQ_info.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.slide_in_right,
-                        R.anim.slide_out_left);
-            }
-        });
-
     }
 
     @Override
     public void onBackPressed()
     {
         super.onBackPressed();
-        Intent intent = new Intent(Earthquake.this, Disaster.class);
+        Intent intent = new Intent(EQ_info.this, Earthquake.class);
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_left,
                 R.anim.slide_out_right);
