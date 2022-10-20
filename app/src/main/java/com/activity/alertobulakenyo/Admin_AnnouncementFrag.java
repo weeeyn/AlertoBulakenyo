081a8db4413ac15712e7a5bd6baac112d6df4a88
weeeyn <rabayareynalyn03@gmail.com>
2022-10-20 11:38:45 +0800

    admin

--- app/src/main/java/com/activity/alertobulakenyo/Admin_AnnouncementFrag.java
+++ app/src/main/java/com/activity/alertobulakenyo/Admin_AnnouncementFrag.java
@@ -63,7 +63,7 @@ public class Admin_AnnouncementFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_admin_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_announcement, container, false);
 
         btnCreatencmnt = view.findViewById (R.id.btnCreateAncmnt);
 
