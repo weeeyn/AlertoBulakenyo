e5d8ad41642b9311c394c5bdf9afeac7d42ce4c2
weeeyn <rabayareynalyn03@gmail.com>
2022-12-05 23:40:37 +0800

    earthquake

--- app/src/main/java/com/activity/alertobulakenyo/Prep_LS.java
+++ app/src/main/java/com/activity/alertobulakenyo/Prep_LS.java
@@ -3,14 +3,21 @@ package com.activity.alertobulakenyo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class Prep_LS extends AppCompatActivity {
+
+    MediaController media;
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,43 @@ public class Prep_LS extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION); //enable full screen
 
         setContentView(R.layout.activity_prep_ls);
+
+        video = (VideoView) findViewById (R.id.video);
+
+        media = new MediaController(this);
+
+        video.setMediaController(media);
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.landslide));
+        video.start();
+
+        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                        video.setMediaController(media);
+                        media.setAnchorView(video);
+
+                    }
+                });
+            }
+        });
+
+        video.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+                return false;
+            }
+        });
+
+        video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+
+                mediaPlayer.release();
+            }
+        });
 
     }
 
--- app/src/main/java/com/activity/alertobulakenyo/Prep_Ty.java
+++ app/src/main/java/com/activity/alertobulakenyo/Prep_Ty.java
@@ -3,11 +3,18 @@ package com.activity.alertobulakenyo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Prep_Ty extends AppCompatActivity {
+
+    MediaController media;
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,43 @@ public class Prep_Ty extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION); //enable full screen
 
         setContentView(R.layout.activity_prep_ty);
+
+        video = (VideoView) findViewById (R.id.video);
+
+        media = new MediaController(this);
+
+        video.setMediaController(media);
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.typhoon_flood));
+        video.start();
+
+        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                        video.setMediaController(media);
+                        media.setAnchorView(video);
+
+                    }
+                });
+            }
+        });
+
+        video.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+                return false;
+            }
+        });
+
+        video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+
+                mediaPlayer.release();
+            }
+        });
     }
 
 
